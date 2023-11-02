10bd82f07652761f3f20fb371d14bf509874eff2
Oferlevy <06ofer@gmail.com>
2023-11-02 17:23:21 +0200

    added option to add a message to the order

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,27 @@
 import { MdOutlineComment } from 'react-icons/md';
 import { useCart } from '@/contexts/CartContext';
 import { useState } from 'react';
+import { IoMdArrowBack } from 'react-icons/io';
 
 import CartItem from '@/components/CartItem';
 
 export default function Cart() {
     const { cart } = useCart();
-    const [placeOrder, setPlaceOrder] = useState(false);
+    const [editMessage, setEditMessage] = useState(false);
+    const [message, setMessage] = useState('');
 
     return (
-        <>
-            <div className='flex flex-col bg-white p-4  h-[38rem] max-h-[85vh]'>
-                <p className='text-2xl font-bold text-center'>ההזמנה שלי</p>
+        <div className='bg-white h-[38rem] max-h-[85vh]'>
+            <div
+                className={`absolute p-4 w-full h-full flex flex-col transition-all ${
+                    editMessage
+                        ? 'opacity-0 pointer-events-none -translate-x-10'
+                        : ''
+                }`}
+            >
+                <p className='text-xl font-bold text-center'>ההזמנה שלי</p>
 
-                <div className='flex flex-row-reverse items-center text-right w-full mt-4'>
+                <div className='flex flex-row-reverse items-center text-right w-full mt-5'>
                     <MdOutlineComment
                         size={22}
                         color='#8b5cf6'
@@ -25,9 +33,17 @@ export default function Cart() {
                             הוסף הערות למנה
                         </p>
                         <span className='text-right text-xs text-gray-500'>
-                            ...סכו״ם, רטבים, וכו
+                            {message || '...סכו״ם, רטבים, וכו'}
                         </span>
                     </div>
+
+                    <div className='flex-1' />
+                    <button
+                        onClick={() => setEditMessage(true)}
+                        className='bg-[#8b5cf6] hover:bg-opacity-30 transition-colors bg-opacity-20 rounded px-4 py-2 text-ms text-[#8b5cf6]'
+                    >
+                        עריכה
+                    </button>
                 </div>
 
                 <div className='my-4 flex-1 overflow-auto divide-y pb-10'>
@@ -38,9 +54,8 @@ export default function Cart() {
 
                 <div className='absolute inset-x-3 bottom-3'>
                     <button
-                        onClick={() => setPlaceOrder(true)}
                         className='flex justify-between bg-[#8b5cf6] w-full
-                 py-3 px-4 mt-2 rounded-md shadow-lg text-white transition'
+                        py-3 px-4 mt-2 rounded-md shadow-lg text-white transition'
                     >
                         <p className=''>{cart.price} ₪</p>
                         <p className='font-semibold'>הזמן</p>
@@ -48,9 +63,37 @@ export default function Cart() {
                 </div>
             </div>
 
-            {/* <Modal isOpen={setPlaceOrder}>
-                <PlaceOrder />
-            </Modal> */}
-        </>
+            <div
+                className={`absolute p-4 w-full h-full transition-all ${
+                    editMessage
+                        ? ''
+                        : 'opacity-0 pointer-events-none translate-x-10'
+                }`}
+            >
+                <button
+                    onClick={() => setEditMessage(false)}
+                    className='absolute bg-gray-200 hover:bg-gray-300 top-3 left-3 rounded-full p-1 transition-colors'
+                >
+                    <IoMdArrowBack size={28} />
+                </button>
+
+                <div className='flex flex-col'>
+                    <p className='mt-11 text-lg font-medium self-end'>
+                        הוסף הערות למנה
+                    </p>
+
+                    <div className='mt-3'>
+                        <textarea
+                            placeholder=' ...סכו״ם, רטבים, וכו'
+                            rows={4}
+                            onChange={(event) => setMessage(event.target.value)}
+                            className='text-right w-full
+                         p-2.5 resize-none outline-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 hover:ring-blue-500 hover:ring-1 hover:border-blue-500
+                         focus:ring-blue-500 focus:ring-1 focus:border-blue-500 transition-all'
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
