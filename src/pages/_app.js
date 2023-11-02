bb6ad2bd463f0d38d5e82f7799a5e7d49c580c50
Oferlevy <06ofer@gmail.com>
2023-11-02 17:30:34 +0200

    fixed "zoom in" bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
+import Head from 'next/head';
 import { CartProvider } from '@/contexts/CartContext';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
     return (
-        <CartProvider>
-            <Component {...pageProps} />
-        </CartProvider>
+        <>
+            <Head>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1, maximum-scale=1'
+                ></meta>
+            </Head>
+
+            <CartProvider>
+                <Component {...pageProps} />
+            </CartProvider>
+        </>
     );
 }
